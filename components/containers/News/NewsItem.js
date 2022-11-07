4082dd2a6a3ab0d844d8d2bde2f3824cb9f89d24
AleksanderSandnes <62210498+AleksanderSandnes@users.noreply.github.com>
2022-11-07 14:53:04 +0100

    News display card
    Initial setup and code for a horizontal scrollable news display card

---
+++ components/containers/News/NewsItem.js
@@ -0,0 +1,73 @@
+import { Text, StyleSheet, TouchableOpacity, Image, View } from 'react-native';
+import { colors } from '../../config/theme';
+import { StatusBar } from 'expo-status-bar';
+
+import StyledText from '../Texts/StyledText'
+
+const NewsItem = ({ image, title, avatar, author, date, ...props }) => {
+    let activeColors = colors;
+
+    return (
+        <TouchableOpacity style={[{ backgroundColor: activeColors.secondary }, styles.container]} {...props}>
+            <Image source={image} style={styles.image} />
+
+            <View style={styles.bottomSection}>
+                <StyledText style={[{ color: activeColors.accent }, styles.title]} bold>{title}</StyledText>
+
+                <View style={styles.authorRow}>
+                    <View style={styles.author}>
+                        <Image source={avatar} style={styles.avatar} />
+                        <StyledText>{author}</StyledText>
+                    </View>
+
+                    <StyledText style={[{ color: activeColors.tertiary }, styles.date]} small>{date}</StyledText>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 370,
+        width: 300,
+        borderRadius: 25,
+        marginRight: 20
+    },
+    image: {
+        width: 300,
+        height: 190,
+        borderRadius: 25
+    },
+    bottomSection: {
+        padding: 25,
+        flex: 1,
+        justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 19
+    },
+    authorRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    author: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginRight: 25,
+        flex: 3
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        marginRight: 10
+    },
+    date: {
+        textAlign: 'right',
+        flex: 2
+    }
+});
+
+export default NewsItem;
