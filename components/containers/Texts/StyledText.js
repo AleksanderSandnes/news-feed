468e9021c56fed971f4a9c875830ce10f4b0f323
AleksanderSandnes <62210498+AleksanderSandnes@users.noreply.github.com>
2022-11-07 15:24:38 +0100

    Prettier script

--- components/containers/Texts/StyledText.js
+++ components/containers/Texts/StyledText.js
@@ -1,20 +1,24 @@
-import { Text } from 'react-native';
-import { colors } from '../../config/theme';
+import { Text } from "react-native";
+import { colors } from "../../config/theme";
 
 const StyledText = ({ children, style, small, big, bold, ...props }) => {
-    let activeColors = colors;
+  let activeColors = colors;
 
-    return (
-        <Text style={[{
-            color: activeColors.tint,
-            fontSize: small ? 14 : big ? 24 : 16,
-            fontWeight: bold || big ? 'bold' : 'normal'
-        }, style]}
-            {...props}
-        >
-            {children}
-        </Text>
-    )
-}
+  return (
+    <Text
+      style={[
+        {
+          color: activeColors.tint,
+          fontSize: small ? 14 : big ? 24 : 16,
+          fontWeight: bold || big ? "bold" : "normal",
+        },
+        style,
+      ]}
+      {...props}
+    >
+      {children}
+    </Text>
+  );
+};
 
 export default StyledText;
--- screens/Home.js
+++ screens/Home.js
@@ -1,26 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 // components
-import MainContainer from '../components/containers/MainContainer';
-import StyledText from '../components/containers/Texts/StyledText';
-import NewsSection from '../components/containers/News/NewsSection';
+import MainContainer from "../components/containers/MainContainer";
+import StyledText from "../components/containers/Texts/StyledText";
+import NewsSection from "../components/containers/News/NewsSection";
 
 // data
-import { newsData } from '../config/data'
+import { newsData } from "../config/data";
 
 export default function Home() {
-    return (
-        <MainContainer>
-            <StyledText style={styles.sectionTitle} big>Trending News</StyledText>
-            <NewsSection data={newsData} />
-        </MainContainer>
-    );
+  return (
+    <MainContainer>
+      <StyledText style={styles.sectionTitle} big>
+        Trending News
+      </StyledText>
+      <NewsSection data={newsData} />
+    </MainContainer>
+  );
 }
 
 const styles = StyleSheet.create({
-    sectionTitle: {
-        marginTop: 25,
-        marginLeft: 25
-    },
+  sectionTitle: {
+    marginTop: 25,
+    marginLeft: 25,
+  },
 });
